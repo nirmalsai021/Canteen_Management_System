e8efc47baf856b7ed509e0a50e062e439c06bb44
Venkat nirmal sai <venket.d.v@gmail.com>
2025-10-22 22:11:43 +0530

    Force deployment - set DEBUG default to False

--- Canteen_Management_System/backend/canteen_cms/settings.py
+++ Canteen_Management_System/backend/canteen_cms/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-23c-w_jq#pvxfjg-ky8^ae8gd$&9w=tm@+bs@hmizp!oep&$%9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost,canteen-backend-bbqk.onrender.com').split(',')
 
